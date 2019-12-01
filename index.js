3af150580207768537411d5738f0fa38c14ff0cb
Anthony Graniero <ananfro@gmail.com>
2019-12-01 18:46:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-let names = []
-
-function writeCards() {
-  
+function writeCards(names, event) {
+  for (let i=0; i<names.length; i++) {
+    console.log(`Thank you, ${names[i]}, for the wonderful ${event[i]} gift!`)
+  } 
 }
